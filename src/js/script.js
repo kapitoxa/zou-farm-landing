3bc43e3da802bd114fe0cc8f331d6559d557f862
Anton Borodaenko <k4p1toxa@gmail.com>
2020-04-24 13:52:21 +0300

    slider for 'how it works' section

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
         prevArrow: '.testimnonials__control_left',
         nextArrow: '.testimnonials__control_right'
     });
+
+    $('.how-it-works__info').slick({
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        swipeToSlide: true,
+        arrows: false,
+        dots: true,
+        dotsClass: 'how-it-works__steps',
+        appendDots: $('.how-it-works__navigation'),
+        customPaging : function(slider, i) {
+            var thumb = $(slider.$slides[i]).data();   
+            return `0${i + 1}`;
+        }
+    });
 });
